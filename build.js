a165bcd75003758dbd5a70450ae7cd71e3292306
Murtaza Aliakbar <murtaza@live.in>
2021-11-27 16:25:29 +0530

    refactor build.js: rmv global vars

--- build.js
+++ build.js
@@ -1,79 +1,71 @@
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path')
 const buildTrie = require("./buildTrie.js")
 
-
-let blocklist = []
-var tag_dict = {}
-var basicconfig = {};
-
-async function getBlockListFiles(path) {
-    let arr = []
-    arr.push(path)
-    while (data = arr.shift()) {
-        const dir = await fs.promises.opendir(data);
-        for await (const dirent of dir) {
-            if (dirent.isDirectory()) {
-                arr.push(data + dirent.name + "/")
-            }
-            else {
-                blocklist.push(data + dirent.name)
+async function getBlocklistFiles(path) {
+    let blocklists = []
+    let dirs = []
+    dirs.push(path)
+    while (const d = dirs.shift()) {
+        const dir = await fs.promises.opendir(d);
+        for await (const entry of dir) {
+            if (entry.isDirectory()) {
+                dirs.push(path.join(d, entry.name))
+            } else {
+                blocklists.push(d + entry.name)
             }
         }
     }
+    return blocklists
 }
-
-
-
 
 async function loadConfig(blocklistConfigPath, unameVnameMapPath) {
     try {
-        var arr = []
-        var fileData = fs.readFileSync(blocklistConfigPath, 'utf8');
-        blocklistobj = JSON.parse(fileData);
-        var mapData = fs.readFileSync(unameVnameMapPath, "utf8")
-        unameVnameMap = JSON.parse(mapData)
-        tag_dict = {}
-        let uname = ""
+        const tags = {}
+        const fileData = fs.readFileSync(blocklistConfigPath, 'utf8')
+        const mapData = fs.readFileSync(unameVnameMapPath, "utf8")
+        const blocklistobj = JSON.parse(fileData);
+        const unameVnameMap = JSON.parse(mapData)
+
         for (let index in blocklistobj.conf) {
-            uname = unameVnameMap[index]
-            if (uname == undefined) {
+            let uname = unameVnameMap[index]
+            if (typeof uname == "undefined") {
                 uname = index + ""
             }
 
-            tag_dict[uname] = {}
-            tag_dict[uname].value = parseInt(index)
-            tag_dict[uname].uname = uname
-            tag_dict[uname].vname = blocklistobj.conf[index].vname
+            tags[uname] = {}
+            tags[uname].value = parseInt(index)
+            tags[uname].uname = uname
+            tags[uname].vname = blocklistobj.conf[index].vname
 
-            tag_dict[uname].group = blocklistobj.conf[index].group
-            tag_dict[uname].subg = blocklistobj.conf[index].subg
-            tag_dict[uname].url = blocklistobj.conf[index].url
-            tag_dict[uname].show = 0
-            tag_dict[uname].entries = 0
-
+            tags[uname].group = blocklistobj.conf[index].group
+            tags[uname].subg = blocklistobj.conf[index].subg
+            tags[uname].url = blocklistobj.conf[index].url
+            tags[uname].show = 0
+            tags[uname].entries = 0
         }
-
-        //fs.writeFileSync("./result/filetag.json", JSON.stringify(tag_dict));
-    }
-    catch (e) {
+        return tags
+    } catch (e) {
         console.log(e)
         throw e
     }
 }
 
 async function main() {
+    const basicconfig = {}
+    const outdir = path.normalize("./result/")
+    const bldir = path.normaize("./blocklistfiles/")
+    const blconfig = path.normalize("./blocklistConfig.json")
+    const unamemap = path.normalize("./valueUnameMap.json")
+
     try {
-
-        await loadConfig("./blocklistConfig.json", "./valueUnameMap.json");
-        await getBlockListFiles('./blocklistfiles/');
-        await buildTrie.build(blocklist, fs, "./result/", tag_dict, basicconfig)
-    }
-    catch (e) {
+        const tags = await loadConfig(blconfig, unamemap)
+        const bl = await getBlocklistFiles(bldir);
+        await buildTrie.build(bl, fs, outdir, tags, basicconfig)
+    } catch (e) {
         console.log(e.stack)
-        node: process.exit(1)
+        process.exitCode = 1
     }
-
 }
-
 
 main()
